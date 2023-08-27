bae20bcbe0b73d747a9826027f0a6a3676509efc
devjunior30 <edyjunior299@gmail.com>
2023-08-27 15:26:33 -0300

    finalização do projeto impar ou par.

--- script.js
+++ script.js
@@ -2,14 +2,18 @@
 
 let text = document.getElementById("paragrafo")
 
+
+
 function soma() {
 
     let numberTree = document.getElementById("input").value
 
-    if (numberTree % 2 ===0) {
+
+    if (numberTree % 2 === 0) {
         paragrafo.innerHTML = "O numero e par 💻"
     } else {
         paragrafo.innerHTML = "O numero e impar 💻"
     }
-    
+
 }
+
